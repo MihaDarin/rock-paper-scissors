66898fb6cffacaa0d5ce739d794c97d60310a9f9
Mike <mihaildaryin@gmail.com>
2021-10-23 22:50:27 +0300

    Edit main.js add function computerPlay and function round

--- main.js
+++ main.js
@@ -2,4 +2,18 @@ function computerPlay (){
     let array = ['Rock', 'Paper', 'Scissors'];
     random = Math.floor(Math.random() * array.length);
     return (random, array[random]);
-}
+};
+console.log(computerPlay());
+
+function round(playerSelection, computerSelection){
+    if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        console.log('You win!');
+    }
+};
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+console.log(round(playerSelection, computerSelection));
+
+function game(){
+
+};

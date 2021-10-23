26a0ade918fc7f1adb76314359515983f442114d
Mike <mihaildaryin@gmail.com>
2021-10-23 23:07:26 +0300

    edit main.js

--- main.js
+++ main.js
@@ -6,14 +6,26 @@ function computerPlay (){
 console.log(computerPlay());
 
 function round(playerSelection, computerSelection){
-    if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors')||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper')||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')){
         console.log('You win!');
+    }else if((playerSelection == 'Paper' && computerSelection == 'Scissors')||
+             (playerSelection == 'Scissors' && computerSelection == 'Rock')||
+             (playerSelection == 'Rock' && computerSelection == 'Paper')){
+        console.log('You lose!');
+    }else{
+        console.log('Nobody win');
     }
 };
-const playerSelection = 'Rock';
+const playerSelection = prompt('Type your choose', '');
 const computerSelection = computerPlay();
 console.log(round(playerSelection, computerSelection));
 
 function game(){
-
+    for (let i=0; i<5; i++){
+        round();
+        console.log(round());
+    }
 };
+console.log(game())

f7eee7d2c079f8ecd4aa7cc6f6e9bfaa64a24590
Mike <mihaildaryin@gmail.com>
2021-11-01 22:31:47 +0300

    center content and add new functions

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ const paper = document.getElementById('paper');
 paper.addEventListener('click', game);
 const scissors = document.getElementById('scissors');
 scissors.addEventListener('click', game);
-let win = document.getElementById('win');
-let lose = document.getElementById('lose');
-let playerScore = 0;
-let computerScore = 0;
+let PlayerScore = document.getElementById('#PlayerScore');
+let ComputerScore = document.getElementById('#ComputerScore');
+const score = document.querySelector('#score');
+const whoWins = document.querySelector('#whoWins');
+
+let playerPoint = 0;
+let computerPoint = 0;
+
+
 function playerSelection(event){
     const target = event.target;
         if (target.tagName != 'BUTTON') return;
       return(target.id)
 };
-
 
 function computerPlay (){
     let array = ['Rock', 'Paper', 'Scissors'];
@@ -25,30 +29,36 @@ function game(playerSelection, computerSelection){
     computerSelection = computerPlay();
     console.log(playerSelection.target.id);
     console.log(computerSelection);   
-    console.log (playerScore);
-    console.log (computerScore);
     if(
         (playerSelection.target.id === 'rock' && computerSelection === 'Scissors')||
         (playerSelection.target.id === 'paper' && computerSelection === 'Rock')||
         (playerSelection.target.id === 'scissors' && computerSelection === 'Paper')){
         console.log('You win!');
-        win.innerHTML++;
-        ++playerScore;
+        ++playerPoint;
+        whoWins.innerHTML = "Player wins";
+        whoWins.classList.add("win");
     }else if(
         (playerSelection.target.id === 'rock' && computerSelection === 'Paper')||
         (playerSelection.target.id === 'paper' && computerSelection === 'Scissors')||
         (playerSelection.target.id === 'scissors' && computerSelection === 'Rock')){
         console.log('You lose!');
-        lose.innerHTML++;
-        ++computerScore;
+        ++computerPoint;
+        whoWins.innerHTML = "Computer wins";
+        whoWins.classList.add("lose");
     }else{
-        console.log('Nobody');
+        console.log('Tie');
+        whoWins.innerHTML = "It's a tie";
+        whoWins.classList.add("tie");
     }
-    
-    if(playerScore == 5){
+
+    score.innerHTML = `<span class="sides">Player: ${playerPoint}</span>
+                   <span class="sides">Computer: ${computerPoint}</span>`;
+
+    if(playerPoint == 5){
         alert ('you win');
-    }else if(computerScore == 5){
+    }else if(computerPoint == 5){
         alert ('computer win');
     }
-    
+    console.log (playerPoint);
+    console.log (computerPoint);
 };

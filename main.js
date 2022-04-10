2fda51a9598631ee70d2a2bf56a87ea963a478f1
Mike <mihaildaryin@gmail.com>
2022-04-10 21:36:46 +0300

    refactoring logic and style

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+// --------------------- Weapons -------------------------//
+
 const rock = document.getElementById("rock");
 rock.addEventListener("click", game);
 const paper = document.getElementById("paper");
 paper.addEventListener("click", game);
 const scissors = document.getElementById("scissors");
 scissors.addEventListener("click", game);
+
+// --------------------- Scores -------------------------//
+
 let PlayerScore = document.getElementById("#PlayerScore");
 let ComputerScore = document.getElementById("#ComputerScore");
 const score = document.querySelector("#score");
@@ -11,6 +16,8 @@ const whoWins = document.querySelector("#whoWins");
 
 let playerPoint = 0;
 let computerPoint = 0;
+
+// --------------------- Player or Computer move -------------------------//
 
 function playerSelection(event) {
   const target = event.target;
@@ -24,52 +31,44 @@ function computerPlay() {
   return random, array[random];
 }
 
+// --------------------- GAME LOGIC-------------------------//
+
 function game(playerSelection, computerSelection) {
   computerSelection = computerPlay();
   if (
-    (playerSelection.target.id === "rock" &&
-      computerSelection === "Scissors") ||
+    (playerSelection.target.id === "rock" && computerSelection === "Scissors") ||
     (playerSelection.target.id === "paper" && computerSelection === "Rock") ||
     (playerSelection.target.id === "scissors" && computerSelection === "Paper")
   ) {
     ++playerPoint;
     whoWins.innerHTML = "Player wins";
-    whoWins.classList.add("win");
-    whoWins.classList.remove("lose");
-    whoWins.classList.remove("tie");
+    whoWins.className = "win";
   } else if (
     (playerSelection.target.id === "rock" && computerSelection === "Paper") ||
-    (playerSelection.target.id === "paper" &&
-      computerSelection === "Scissors") ||
+    (playerSelection.target.id === "paper" && computerSelection === "Scissors") ||
     (playerSelection.target.id === "scissors" && computerSelection === "Rock")
   ) {
     ++computerPoint;
     whoWins.innerHTML = "Computer wins";
-    whoWins.classList.remove("win");
-    whoWins.classList.remove("tie");
-    whoWins.classList.add("lose");
+    whoWins.className = "lose";
   } else {
     whoWins.innerHTML = "It's a tie";
-    whoWins.classList.remove("win");
-    whoWins.classList.remove("lose");
-    whoWins.classList.add("tie");
+    whoWins.className = "tie";
   }
 
   score.innerHTML = `<span class="sides">Player: ${playerPoint}</span>
-                   <span class="sides">Computer: ${computerPoint}</span>`;
+                     <span class="sides">Computer: ${computerPoint}</span>`;
+
+  // --------------------- WINNER RESULT-------------------------//
 
   if (playerPoint == 5) {
     whoWins.innerHTML = "Congratulations! You win!";
-    whoWins.classList.add("win");
-    whoWins.classList.remove("lose");
-    whoWins.classList.remove("tie");
+    whoWins.className = "win";
     playerPoint = 0;
     computerPoint = 0;
   } else if (computerPoint == 5) {
     whoWins.innerHTML = "End Game! You lose!";
-    whoWins.classList.remove("win");
-    whoWins.classList.remove("tie");
-    whoWins.classList.add("lose");
+    whoWins.className = "lose";
     playerPoint = 0;
     computerPoint = 0;
   }
